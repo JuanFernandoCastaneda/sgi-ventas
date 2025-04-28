1b681a4ec92906502e26b347760e9a7e1e6cc1c3
JuanFernandoCastaneda <jf.castaneda@uniandes.edu.co>
2025-04-28 15:23:42 -0500

    Agregado endpoint para formas de pago

--- ventas-back/app/main.py
+++ ventas-back/app/main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
-from app.routers import ordenes_router, productos_ordenes_router
+from app.routers import ordenes_router, productos_ordenes_router, forma_pago_router
 
 app = FastAPI()
 
 app.include_router(ordenes_router.router)
 app.include_router(productos_ordenes_router.router)
+
+app.include_router(forma_pago_router.router)
