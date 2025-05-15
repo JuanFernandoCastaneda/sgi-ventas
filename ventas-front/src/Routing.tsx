0d5e0013a4f2207d7adc96f05c1a7c1d6bcf397c
JuanFernandoCastaneda <jf.castaneda@uniandes.edu.co>
2025-05-15 08:51:26 -0500

    Added layout skeleton

--- ventas-front/src/Routing.tsx
+++ ventas-front/src/Routing.tsx
@@ -2,15 +2,18 @@ import { Navigate, Route, Routes } from "react-router";
 import { CrearOrden } from "./pages/CrearOrden";
 import { ListaOrdenes } from "./pages/ListaOrdenes";
 import { VerOrden } from "./pages/VerOrden";
+import { Layout } from "./layouts/Layout";
 
 export function Routing() {
   return (
     <Routes>
-      <Route index element={<Navigate to="/ordenes" />} />
-      <Route path="ordenes">
-        <Route index element={<ListaOrdenes />} />
-        <Route path=":id" element={<VerOrden />} />
-        <Route path="crearOrden" element={<CrearOrden />} />
+      <Route element={<Layout />}>
+        <Route index element={<Navigate to="/ordenes" />} />
+        <Route path="ordenes">
+          <Route index element={<ListaOrdenes />} />
+          <Route path=":id" element={<VerOrden />} />
+          <Route path="crearOrden" element={<CrearOrden />} />
+        </Route>
       </Route>
     </Routes>
   );
